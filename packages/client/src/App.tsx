428ae9c06bddab1d27b5b0346e168c755f9b67d7
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2025-12-28 15:33:27 +0100

    setup shadcn ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -9,6 +10,11 @@ function App() {
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
-  return <p className="font-bold p-4 text-3xl">{message}</p>;
+  return (
+    <div className="p-4">
+      <p className="font-bold text-3xl">{message}</p>
+      <Button>Click me</Button>
+    </div>
+  );
 }
 export default App;
