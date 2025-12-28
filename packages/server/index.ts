cea8944d14d00f27112e67e6a52a0bebe8180004
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2025-12-28 10:42:37 +0100

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,5 +10,8 @@ const port = process.env.port || 3000;
 app.get('/', (req:Request ,res:Response) => {
     res.send('Hello World');
 })
+app.get('/api/hello', (req:Request ,res:Response) => {
+    res.json({ message: 'Hello world'});
+})
 
 app.listen(port, () => console.log(`Server is running on http:localhost:${port}`))
