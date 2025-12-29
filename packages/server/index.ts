0f1587108cca358dfeba5bf5fec4c805256c9878
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2025-12-29 19:21:42 +0100

    set up prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,11 +7,13 @@ dotenv.config();
 const app = express();
 const port = process.env.port || 3000;
 
-app.get('/', (req:Request ,res:Response) => {
-    res.send('Hello World');
-})
-app.get('/api/hello', (req:Request ,res:Response) => {
-    res.json({ message: 'Hello world'});
-})
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World');
+});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Hello world' });
+});
 
-app.listen(port, () => console.log(`Server is running on http:localhost:${port}`))
+app.listen(port, () =>
+   console.log(`Server is running on http:localhost:${port}`)
+);
