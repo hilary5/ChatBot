595697e5ea3779aaab821c4a64b6bc1e504108cc
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2026-01-04 10:37:25 +0100

    Revert "fixing ui errors"
    This reverts commit 4df97c4885ebafd324e3d1febdc8f2b3349522b3.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
+import { useEffect } from 'react';
 import ChatBox from './components/chat/ChatBox';
 
 function App() {
+   useEffect(() => {
+      const setVh = () => {
+         document.documentElement.style.setProperty(
+            '--vh',
+            `${window.innerHeight * 0.01}px`
+         );
+      };
+      setVh();
+      window.addEventListener('resize', setVh);
+      return () => window.removeEventListener('resize', setVh);
+   }, []);
+
    return (
-      <div className="p-4 w-full self-center mx-auto flex flex-col">
+      <div
+         className="p-4 w-full self-center mx-auto flex flex-col"
+         style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
+      >
          <ChatBox />
       </div>
    );
