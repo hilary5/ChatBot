3c1c9c181a8a131dd779e5ba935b69bc9f1adf97
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2026-01-04 02:38:01 +0100

    re updated

--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -1,5 +1,5 @@
 import OpenAI from 'openai';
-import { conversationRepository } from '../repositories/conversation.repository';
+import { conversationRepository } from '../repositories/conversation.repository.ts';
 
 const client = new OpenAI({
    apiKey: process.env.OPENAI_API_KEY,
