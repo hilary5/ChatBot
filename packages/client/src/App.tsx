db4988d1b694c76d5980efee23d1e611544d7889
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2026-01-04 01:31:06 +0100

    improving chatbot responses

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Button } from './components/ui/button';
+import ChatBox from './components/chat/ChatBox';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
    return (
-      <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me</Button>
+      <div className="p-4 h-screen w-full self-center mx-auto">
+         <ChatBox />
       </div>
    );
 }
---
+++ packages/client/src/components/chat/ChatBox.tsx
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { useRef, useState } from 'react';
+import ChatInput, { type ChatFormData } from './ChatInput';
+import type { Message } from './ChatMessages';
+import ChatMessages from './ChatMessages';
+import TypingIndicator from './TypingIndicator';
+import popSound from '@/assets/sounds/pop.mp3';
+import notificationSound from '@/assets/sounds/notification.mp3';
+
+const popAudio = new Audio(popSound);
+popAudio.volume = 0.2;
+
+const notificationAudio = new Audio(notificationSound);
+notificationAudio.volume = 0.2;
+
+type ChatResponse = {
+   message: string;
+};
+
+const ChatBox = () => {
+   const [messages, setMessages] = useState<Message[]>([]);
+   const [isBotTyping, setIsBotTyping] = useState(false);
+   const [error, setError] = useState('');
+
+   const conversationId = useRef(crypto.randomUUID());
+
+   const onSubmit = async ({ prompt }: ChatFormData) => {
+      try {
+         setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
+         setIsBotTyping(true);
+         setError('');
+         popAudio.play();
+
+         const { data } = await axios.post<ChatResponse>('/api/chat', {
+            prompt,
+            conversationId: conversationId.current,
+         });
+         setMessages((prev) => [
+            ...prev,
+            { content: data.message, role: 'bot' },
+         ]);
+         notificationAudio.play();
+      } catch (error) {
+         console.log(error);
+         setError('Something went wrong. Please try again.');
+      } finally {
+         setIsBotTyping(false);
+      }
+   };
+
+   return (
+      <div className="flex flex-col h-full max-w-2xl mx-auto">
+         <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto max-w-2xl">
+            {<ChatMessages messages={messages} />}
+            {isBotTyping && <TypingIndicator />}
+            {error && <div className="text-red-500">{error}</div>}
+         </div>
+         <ChatInput onSubmit={onSubmit} />
+      </div>
+   );
+};
+
+export default ChatBox;
