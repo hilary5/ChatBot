982997391ee23fc6f1d200478561584dae64ca0b
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2026-01-04 10:38:08 +0100

    Revert "updated app.tsx useEffect hook"
    This reverts commit 8d622ef9719898f251e0f5ab0bbee9d383f763e0.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,28 +1,11 @@
 import './App.css';
-import { useEffect } from 'react';
 import ChatBox from './components/chat/ChatBox';
 
 function App() {
-   useEffect(() => {
-      const setVh = () => {
-         document.documentElement.style.setProperty(
-            '--vh',
-            `${window.innerHeight * 0.01}px`
-         );
-      };
-      setVh();
-      window.addEventListener('resize', setVh);
-      return () => window.removeEventListener('resize', setVh);
-   }, []);
-
    return (
-      <div
-         className="p-4 w-full self-center mx-auto flex flex-col"
-         style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
-      >
+      <div className="p-4 h-screen w-full self-center mx-auto">
          <ChatBox />
       </div>
    );
 }
-
 export default App;
