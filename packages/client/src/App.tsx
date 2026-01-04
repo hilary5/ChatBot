4df97c4885ebafd324e3d1febdc8f2b3349522b3
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2026-01-04 10:28:00 +0100

    fixing ui errors

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,9 @@
 import './App.css';
-import { useEffect } from 'react';
 import ChatBox from './components/chat/ChatBox';
 
 function App() {
-   useEffect(() => {
-      const setVh = () => {
-         document.documentElement.style.setProperty(
-            '--vh',
-            `${window.innerHeight * 0.01}px`
-         );
-      };
-      setVh();
-      window.addEventListener('resize', setVh);
-      return () => window.removeEventListener('resize', setVh);
-   }, []);
-
    return (
-      <div
-         className="p-4 w-full self-center mx-auto flex flex-col"
-         style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
-      >
+      <div className="p-4 w-full self-center mx-auto flex flex-col">
          <ChatBox />
       </div>
    );
